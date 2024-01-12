b6076ec318fa0d139428b81d02b5b623da0224c4
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-01-12 16:02:38 +0530

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,18 +4,65 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import AllowAny
 from django.shortcuts import render, redirect
 from rest_framework.response import Response
+import sys, threading, time, os, io, locale
 from django.db.models import Max, Min
 from django.http import JsonResponse
 from rest_framework import status
 from .models import *
 import pandas as pd
-import sys
-import threading
-import time
-import os
-import io
 
 # Create your views here.
+def indian_format(number):
+    locale.setlocale(locale.LC_ALL, 'en_IN.UTF-8')
+    formatted_number = locale.format_string("%d", number, grouping=True)
+    return formatted_number
+
+def upload_chunk_data(chunk):
+    print("upload_chunk_data started")
+    try:
+        # this function is called by the threads to read the files and upload data inside them chunk by chunk
+        chunk = chunk.rename(columns={
+            'Unnamed: 0':'unique_number',
+            'name':'company_name',
+            'year founded': 'year_founded', 
+            'size range': 'size_range',
+            'linkedin url': 'linkedin_url',
+            'current employee estimate': 'current_employee_estimate',
+            'total employee estimate': 'total_employee_estimate'
+        })
+        chunk[['city', 'state', 'country_2']] = chunk['locality'].str.split(',', expand=True)
+        chunk.drop(columns="country_2", inplace=True)
+        # chunk['year_founded'].fillna(value = 0, inplace=True)                
+        # chunk['current_employee_estimate'].fillna(value = 0, inplace=True)            
+        chunk.dropna()    
+        instances = [companiesModel(
+            unique_number = row['unique_number'],
+            company_name = str(row['company_name']).title(),
+            domain = row['domain'],
+            year_founded = row['year_founded'],
+            industry = str(row['industry']).title(),
+            size_range = row['size_range'],
+            locality = str(row['locality']).title(),
+            city = str(row['city']).title(),
+            state = str(row['state']).title(),
+            country = str(row['country']).title(),
+            linkedin_url = row['linkedin_url'],
+            current_employee_estimate = row['current_employee_estimate'],
+            total_employee_estimate = row['total_employee_estimate']
+        ) for index, row in chunk.iterrows()]
+        print("Bulk Create Started")
+        companiesModel.objects.bulk_create(instances)
+        print("Bulk Create Ended")
+    except Exception as e:
+        print(e)
+        exception_type, exception_object, exception_traceback = sys.exc_info()
+        filename = exception_traceback.tb_frame.f_code.co_filename
+        line_number = exception_traceback.tb_lineno
+        print("Exception type: ", exception_type)
+        print("File name: ", filename)
+        print("Line number: ", line_number)
+        
+
 
 @csrf_exempt
 @login_required(login_url='account_login')
@@ -33,36 +80,6 @@ def upload_data(request):
             filesUploadModelObj.save()
             # response_message = "File Uploaded Successfully"
 
-            def upload_chunk_data(chunk):
-                # this function is called by the threads to read the files and upload data inside them chunk by chunk
-                chunk = chunk.rename(columns={
-                    'Unnamed: 0':'unique_number',
-                    'name':'company_name',
-                    'year founded': 'year_founded', 
-                    'size range': 'size_range',
-                    'linkedin url': 'linkedin_url',
-                    'current employee estimate': 'current_employee_estimate',
-                    'total employee estimate': 'total_employee_estimate'
-                })
-                chunk[['city', 'state', 'country_2']] = chunk['locality'].str.split(',', expand=True)
-                chunk.drop(columns="country_2", inplace=True)
-                chunk['year_founded'].fillna(value = 0, inplace=True)                
-                instances = [companiesModel(
-                    unique_number = row['unique_number'],
-                    company_name = str(row['company_name']).title(),
-                    domain = row['domain'],
-                    year_founded = row['year_founded'],
-                    industry = str(row['industry']).title(),
-                    size_range = row['size_range'],
-                    locality = str(row['locality']).title(),
-                    city = str(row['city']).title(),
-                    state = str(row['state']).title(),
-                    country = str(row['country']).title(),
-                    linkedin_url = row['linkedin_url'],
-                    current_employee_estimate = row['current_employee_estimate'],
-                    total_employee_estimate = row['total_employee_estimate']
-                ) for index, row in chunk.iterrows()]
-                companiesModel.objects.bulk_create(instances)
 
             parent_df=pd.read_csv(filesUploadModelObj.uploaded_file, iterator=True, chunksize=100000)
             threads = []
@@ -89,14 +106,21 @@ def upload_data(request):
                 # Create multiple threads
                 thread = threading.Thread(target=upload_chunk_data, args=(chunk,))
                 threads.append(thread)
+                
+            print("All threads created")
+            print("Number of threads",threading.active_count())
 
             # Start the threads
             for thread in threads:
                 thread.start()
 
+            print("Number of threads",threading.active_count())
+            print("All threads started")
+
             # Wait for all threads to finish
             for thread in threads:
                 thread.join()
+            print("Number of threads",threading.active_count())
             
             response_message = "Data uploaded successfully"
             print("All threads finished")
@@ -129,16 +153,17 @@ def query_builder(request):
     companiesModelObj = companiesModel.objects.all()
     context = {}
     context['city'] = companiesModelObj.values_list('city', flat=True).distinct()
+    print("cities number",len(context['city']))
     context['state'] = companiesModelObj.values_list('state', flat=True).distinct()
     context['countryList'] = companiesModelObj.values_list('country', flat=True).distinct()
     context['industry'] = companiesModelObj.values_list('industry', flat=True).distinct()
     context['year_founded'] = companiesModelObj.values_list('year_founded', flat=True).distinct()
 
 
-    max_value = companiesModelObj.aggregate(max_value=Max('total_employee_estimate'))['max_value']
-    min_value = companiesModelObj.aggregate(min_value=Min('total_employee_estimate'))['min_value']
-    print("Maximum value:", max_value)    
-    print("Minimum value:", min_value)
+    # max_value = companiesModelObj.aggregate(max_value=Max('total_employee_estimate'))['max_value']
+    # min_value = companiesModelObj.aggregate(min_value=Min('total_employee_estimate'))['min_value']
+    # print("Maximum value:", max_value)    
+    # print("Minimum value:", min_value)
     return render(request, 'query_builder.html', context)
 
 @csrf_exempt
@@ -154,26 +179,31 @@ def query_api(request):
             city = request.POST.get('city')
             state = request.POST.get('state')
             country = request.POST.get('country')
-            total_employee_estimate = request.POST.get('total_employee_estimate')
 
             companiesModelObj = companiesModel.objects.all()
+            
+            data_dict={}
+            
+            if company_name not in [None, '',' ','None']:
+                data_dict['company_name__icontains']=company_name
+            if year_founded !="NA":
+                data_dict['year_founded']=year_founded
+            if industry !="NA":
+                data_dict['industry__icontains']=industry
+            if city !="NA":
+                data_dict['city__icontains']=city
+            if state !="NA":
+                data_dict['state__icontains']=state
+            if country !="NA":
+                data_dict['country__icontains']=country
 
-            if total_employee_estimate!="NA":
-                companiesModelObj = companiesModelObj.filter(total_employee_estimate = total_employee_estimate)
-            if company_name not in ["NA",'', None]:
-                companiesModelObj = companiesModelObj.filter(company_name__in = company_name)
-            if industry!="NA":
-                companiesModelObj = companiesModelObj.filter(industry__in = industry)
-            if city != "NA":
-                companiesModelObj = companiesModelObj.filter(city__in = city)
-            if state != "NA":
-                companiesModelObj = companiesModelObj.filter(state__in = state)
-            if country != "NA":
-                companiesModelObj = companiesModelObj.filter(country__in = country)
-            if year_founded!="NA" and type(year_founded)=='int':
-                companiesModelObj = companiesModelObj.filter(year_founded = year_founded)
-
-            context={"message":f"There are {companiesModelObj.count()} object for the records"}
+            companiesModelObj = companiesModelObj.filter(**data_dict)
+            
+            context={
+                "message":f"There are {indian_format(companiesModelObj.count())} object for the records",
+                "data":companiesModelObj.values()            
+            }
+            print("Content processed")
         except Exception as e:
             print("Error ",e)
             exception_type, exception_object, exception_traceback = sys.exc_info()
